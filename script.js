63d3fbe44b3d5efe43db87a3aca7b880ddc92924
Danny Regan <d.regan@columbia.edu>
2023-12-29 15:03:37 -0500

    countVowels function works properly

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ const input = document.getElementById('string');
 const btn = document.getElementById('submit');
 const output = document.getElementById('output');
 
+
+
 function countVowels(event) {
     event.preventDefault();
-    output.innerHTML = input.value;
+    const string = input.value;
+    let count = 0;
+    let array = [...string];
+    for (let i=0; i<array.length; i++) {
+        if (array[i] === 'a' || array[i] === 'e' || array[i] === 'i' || array[i] === 'o' || array[i] === 'u') {
+            count += 1;
+        }
+    }
+    return output.innerHTML = count;
 }
 
 btn.addEventListener('click', countVowels);
-
-/* 
-asdfasdfasf
-make into array
-[a,s,d,f,a,s,d,f,a,s,f]
-for loop to go through and look for a,e,i,o,u
-add them up
-*/
